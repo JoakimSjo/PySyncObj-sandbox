7a7466b4e9d7542aa108bcdf8a150a6f8d769fec
Joakim Sjøhaug <joakim@sjoha.ug>
2020-03-21 17:57:21 +0100

    wip

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from flask import Flask, request
-from pysyncobj import SyncObj, replicated
+from pysyncobj import SyncObj, replicated, SyncObjConf
 from pysyncobj.batteries import ReplCounter
 import json
 import os
@@ -12,12 +12,12 @@ TOTAL_NODES = int(os.environ["TOTAL_NODES"])
 ADDRESSES = ["node" + str(n) + ":" + RAFT_PORT for n in range(TOTAL_NODES)]
 MY_ADDRESS = ADDRESSES[MY_NODE_NUM]
 PEER_ADDRESSES = [ADDRESSES[i] for i in range(TOTAL_NODES) if i != MY_NODE_NUM]
-
+print(PEER_ADDRESSES)
 counter = ReplCounter()
-sync = SyncObj(MY_ADDRESS, PEER_ADDRESSES, consumers=[counter])
-sync.waitBinded()
+
 
 app = Flask(__name__)
+sync = None
 
 
 @app.route("/inc")
@@ -27,8 +27,41 @@ def inc():
 
 @app.route("/status")
 def status():
+    global sync
+    if sync is None:
+        return "Not started"
+
     return json.dumps(sync.getStatus())
 
 
+@app.route("/start", methods=["POST"])
+def start():
+    global sync
+    app.logger.info("starting")
+    data = request.get_json()
+    peers = [] if "peers" not in data else data["peers"]
+    app.logger.info(peers)
+    conf = SyncObjConf(dynamicMembershipChange=True)
+    sync = SyncObj(MY_ADDRESS, peers, consumers=[counter], conf=conf)
+    sync.waitBinded()
+    app.logger.info("started")
+    return "Started"
+
+
+@app.route("/join", methods=["POST"])
+def join():
+    global sync
+    if sync is None:
+        return "Not started"
+
+    data = request.get_json()
+    join = data["url"]
+
+    app.logger.info(data["url"])
+    sync._addNodeToCluster(join, lambda res, error: app.logger.info(res))
+    return "Ok"
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="500" + str(MY_NODE_NUM))
+    print(MY_ADDRESS)
+    app.run(host="0.0.0.0",  debug=True, port="500" + str(MY_NODE_NUM))
