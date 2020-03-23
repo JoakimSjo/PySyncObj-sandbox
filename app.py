8f65014927807419d3b41d06d8d4105071f6268e
Joakim Sjøhaug <joakim@sjoha.ug>
2020-03-23 11:25:04 +0100

    Fix dynamic joining example

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from pysyncobj.batteries import ReplCounter
 import json
 import os
 import socket
-import threading
+import sys
+from threading import Thread
+from queue import Queue
 from time import sleep
 import requests
 # Dynamic join procedure
@@ -22,86 +24,125 @@ MY_ADDRESS = ADDRESSES[MY_NODE_NUM]
 PEER_ADDRESSES = [ADDRESSES[i] for i in range(TOTAL_NODES) if i != MY_NODE_NUM]
 nodes = []
 
+web = Queue()
+worm = Queue()
+
 
 def createNode(hostname, nodes=[]):
     counter = ReplCounter()
+
     conf = SyncObjConf(dynamicMembershipChange=True)
     sync = SyncObj(MY_ADDRESS, nodes, consumers=[counter], conf=conf)
     sync.waitBinded()
-    return sync
+    return sync, counter
 
 
 app = Flask(__name__)
 app.secret_key = "notsosecret"
 node = None
 
-# Join after different times
-sleep(10 * MY_NODE_NUM)
-if MY_NODE_NUM == 0:
-    node = createNode(ADDRESSES[0])
-    node.addNodeToCluster(ADDRESSES[1], lambda a, b: print(f"a: {a} ,b: {b}"))
 
-if MY_NODE_NUM == 1:
-    # 1. /join -> "node1:4321"
-    # 2. addNodeToCluster "node1:4321"
-    # 3. -> ["node0:4321"]
-    # 4. SyncObj("node1:4321", ["node0:4321", "node2:4321"])
-    # /join => [node0:4321, node1:4321]
-    node = createNode(ADDRESSES[1], nodes=[ADDRESSES[0]])
-    node.addNodeToCluster(ADDRESSES[2])
-elif MY_NODE_NUM == 2:
-    node = createNode(ADDRESSES[2], nodes=ADDRESSES[:2])
-#sync._addNodeToCluster(ADDRESSES[0], lambda a, b: print(f"a: {a} ,b: {b}"))
-elif MY_NODE_NUM != 0:
-    node = createNode(ADDRESSES[MY_NODE_NUM])
-#cache = {}
+def worm_th(web_in, worm_out):
+    node = None
+    counter = None
+    nodes = []
+    while True:
+        # print("waiting on message",  file=sys.stderr)
+        print("waiting on message", flush=True)
+        # Wait on command
+        msg = worm_out.get()
+        # print("Got message",  file=sys.stderr)
+        print("Got message", flush=True)
+        # print(msg, file=sys.stderr)
+        print(msg, flush=True)
+        command = msg["command"]
+        data = msg["data"]
+        # print(command,  file=sys.stderr)
+        print(command, flush=True)
+        try:
+            if command == "join":
+                node.addNodeToCluster(data)
+                others = nodes.copy()
+                nodes.append(data)
+                web_in.put(others)
+            elif command == "start":
+                node, counter = createNode(MY_ADDRESS, data)
+                nodes = data + [MY_ADDRESS]
+                web_in.put(nodes)
+            elif command == "nodes":
+                web_in.put(nodes)
+            elif command == "status":
+                web_in.put({"cluster": node.getStatus(),
+                            "counter": counter.get()})
+            elif command == "leave":
+                web_in.put("leaving")
+            elif command == "inc":
+                web_in.put(counter.inc(sync=True))
+
+        except Exception as e:
+            print(e)
+        # sleep(2)
+
+
+t1 = Thread(target=worm_th, args=(web, worm))
+t1.start()
 
 
 @app.route("/start", methods=["POST"])
 def start():
+    # print("start",  file=sys.stderr)
+    print("start", flush=True)
+    msg = {"command": "start", "data": []}
     if MY_NODE_NUM == 0:
-        cache['node'] = createNode(MY_ADDRESS, [])
-        cache['nodes'] = [MY_ADDRESS]
+        worm.put(msg)
+        data = web.get()
     else:
         data = request.get_json()
         node_api = data["node_api"]
-        print(requests.get(f"http://{node_api}/status".strip()))
+
         response = requests.post("http://" + node_api +
                                  '/join', json={"url": MY_ADDRESS})
-        print(response)
+
+        print(response, flush=True)
         others = response.json()
-        cache['nodes'] = others + [MY_ADDRESS]
-        print(others)
-        cache['node'] = createNode(MY_ADDRESS, others)
-    # sleep(4)
-    #cache.modified = True
-    return json.dumps(cache['nodes'])
+        msg["data"] = others
+        worm.put(msg)
+        data = web.get()
+
+    return json.dumps(data)
 
 
 @app.route("/join", methods=["POST"])
 def join():
-    #global node, nodes
-    data = request.get_json()
-    joiner = data["url"]
-    others = cache['nodes'].copy()
-    cache['nodes'].append(joiner)
-    cache['node'].addNodeToCluster(joiner)
-    # sleep(10)
+    msg = {"command": "join", "data": []}
 
-    #cache.modified = True
-    return json.dumps(others)
+    data = request.get_json()
+    print(data, flush=True)
+    msg["data"] = data["url"]
+
+    worm.put(msg)
+    nodes = web.get()
+
+    # cache.modified = True
+    return json.dumps(nodes)
 
 
 @app.route("/inc")
 def inc():
-    return json.dumps(counter.inc(sync=True))
+    msg = {"command": "inc", "data": []}
+    worm.put(msg)
+    data = web.get()
+    return json.dumps(data)
 
 
 @app.route("/status")
 def status():
-    #global node
-    return json.dumps(node.getStatus())
+    msg = {"command": "status", "data": []}
+    worm.put(msg)
+    data = web.get()
+    # global node
+    return json.dumps(data)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="500" + str(MY_NODE_NUM))
+    app.run(host="0.0.0.0", debug=True, port="500" + str(MY_NODE_NUM))
